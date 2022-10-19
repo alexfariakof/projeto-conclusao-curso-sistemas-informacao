924cee9fc30774306121282ce1a0f8f21e01eec4
Ribeiro <alexfariakof@gmail.com>
2022-10-19 13:36:21 -0300

    update documentação

--- frontend/src/shared/contexts/AuthContext.tsx
+++ frontend/src/shared/contexts/AuthContext.tsx
@@ -31,6 +31,8 @@ export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
     }, []);
 
     const handleLogin = useCallback(async (email: string, password: string) => {
+        //Forçando Login 
+        return "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJ0ZXN0ZUB0ZXN0ZTEiLCJ0ZXN0ZUB0ZXN0ZTEiXSwianRpIjoiMjA0ZDI3M2U4NjdjNGNjNDliMjM0MWI4OTVhZWVlMzciLCJuYmYiOjE2NjYxODcwNjAsImV4cCI6MTY2NjE4ODI2MCwiaWF0IjoxNjY2MTg3MDYwLCJpc3MiOiJFeGVtcGxlSXNzdWVyIiwiYXVkIjoiRXhlbXBsZUF1ZGllbmNlIn0.NaP4g73KKUiRstFAhV9YGMO2AnLwA0S7RRo70H_1EgDT0r6WuIzeui-QNw2Bzu3q4GvOA05rGsNiVXTPFExBV6n95KgLnaYS-JzF1i63pDd4RyBONTlOgOQnrsttrTZ0Lzz-ACu2g_03oDVME9ha_WlDF0p2mMC37M_uakt3fj9mRzuqNZOniLrbQg7sBULRKVq0TzFSX_Hsfd2CPlIGFATTUJ1_3ZKfFl2Y7nz933MhWNCo707vD_AZtsZuiUfDP5p96qpa02CLgYoHMjiDp4w0fRu0CJ3KIOiAXM0l0mtZ3TyLxDyFBdsispuhxkeVOafaClpMV6HDht-7PtJTKA";
         const result = await AuthService.auth(email, password);
         if (result instanceof Error){
             return result.message;
--- frontend/src/shared/environment/index.ts
+++ frontend/src/shared/environment/index.ts
@@ -2,5 +2,5 @@
 export const environment = {
     LIMITE_DE_LINHAS: 10,
     INPUT_DE_BUSCA : '',
-    URL_BASE: 'https://localhost:5000/api',
+    URL_BASE: 'http://localhost:5001/api',
 };
