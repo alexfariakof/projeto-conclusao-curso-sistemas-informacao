2afb70c862a757884ab9dd65b18de2fbe2055fed
Ribeiro <alexfariakof@gmail.com>
2022-10-24 18:48:02 -0300

    Ajustes no desing da dshboar

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,20 +5,21 @@ import { BarCharts, BarraFerramentas } from '../shared/components';
 export const Dashboard = () => {
     return (
         <LayoutMasterPage 
-        titulo='Meu Dashboard' 
+        titulo='Dashboard' 
         barraDeFerramentas={(
             <BarraFerramentas btnNovo={false} btnVoltar={false}  />
           )}
         > 
             <Box gap={1}
-                margin={2}
+                margin={1}
                 padding={1}
                 paddingX={2}
                 height="100%"
                 display="flex"
                 flexDirection="column"
                 alignItems="start"
-                component={Paper}>
+                component={Paper} 
+                >
                     <BarCharts />
             </Box>
         </LayoutMasterPage>
